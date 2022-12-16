2d67b2d06162d23be0018e62866c4bcb871c0cab
Brick-C <tamtop2000@gmail.com>
2022-12-16 12:08:38 +0600

    style name change

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const input = document.querySelector('.heading')
-const align = document.querySelectorAll('.align')
+const input2 = document.querySelector('#my-text')
+const alignh = document.querySelectorAll('.align')
+const alignb = document.querySelectorAll('.alignb')
 const h1 = document.querySelector('.holder')
+const p = document.querySelector('.store')
 const size = document.querySelector('.size')
-const color = document.querySelector('.form-control-color')
-const ff = document.querySelector('.ff')
+const sizeb = document.querySelector('.sizeb')
+const colorh = document.querySelector('.form-control-hcolor')
+const colorb = document.querySelector('.form-control-bcolor')
+const ffh = document.querySelector('.ffh')
+const ffb = document.querySelector('.ffb')
 
 input.onkeyup = () => {
     h1.innerHTML = input.value
 }
 
-align.forEach(item => {
+input2.onkeyup = () => {
+    p.innerHTML = input2.value
+}
+
+alignh.forEach(item => {
     item.onchange = () =>{
         h1.style.textAlign = item.value
+    }
+});
+
+alignb.forEach(item => {
+    item.onchange = () =>{
+        
+        p.style.textAlign = item.value
     }
 });
 
@@ -19,11 +36,22 @@ size.oninput = () => {
     h1.style.fontSize = size.value + 'px'
 }
 
-color.oninput = () => {
-    h1.style.color = color.value
+sizeb.oninput = () => {
+    p.style.fontSize = sizeb.value + 'px'
+}
+colorh.oninput = () => {
+    h1.style.color = colorh.value
 }
 
-ff.onchange = () => {
-    h1.style.fontFamily = ff.value
+colorb.oninput = () => {
+    p.style.color = colorb.value
+}
+
+ffh.onchange = () => {
+    h1.style.fontFamily = ffh.value
+}
+
+ffb.onchange = () => {
+    p.style.fontFamily = ffb.value
 }
 
